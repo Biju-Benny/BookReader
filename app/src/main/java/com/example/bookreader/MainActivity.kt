d29136dec181208a5f63307d16c055e646813acb
Biju-Benny <70836378+Biju-Benny@users.noreply.github.com>
2022-03-09 20:03:21 +0530

    Second Commit

--- app/src/main/java/com/example/bookreader/MainActivity.kt
+++ app/src/main/java/com/example/bookreader/MainActivity.kt
@@ -1,30 +1,39 @@
 
 package com.example.bookreader
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.example.bookreader.Adapters.ViewPagerAdapter
 import com.example.bookreader.Fragments.FavouritesFragment
 import com.example.bookreader.Fragments.HomeFragment
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.auth.User
 import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.nav_header.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var toggle : ActionBarDrawerToggle
     private lateinit var firebaseDb: FirebaseFirestore
+    lateinit var drawerLayout : DrawerLayout
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth
+
 
         val uidCurrent = FirebaseAuth.getInstance().uid ?: ""
         firebaseDb = FirebaseFirestore.getInstance()
@@ -36,7 +45,7 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        val drawerLayout : DrawerLayout= findViewById(R.id.drawerLayout)
+        drawerLayout = findViewById(R.id.drawerLayout)
         val navView: NavigationView = findViewById(R.id.navView)
 
         toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
@@ -48,16 +57,25 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         navView.setNavigationItemSelectedListener {
+            it.isChecked = true
             when(it.itemId){
-                R.id.nav_home -> Toast.makeText(applicationContext, "Clicked Home", Toast.LENGTH_SHORT).show()
-                R.id.nav_Fav -> Toast.makeText(applicationContext, "Clicked Favourites", Toast.LENGTH_SHORT).show()
-                R.id.nav_logout -> Toast.makeText(applicationContext, "logout", Toast.LENGTH_SHORT).show()
+
+                R.id.nav_logout -> signoutFunction()
             }
             true
         }
 
+
         setupTabs()
     }
+
+    private fun signoutFunction() {
+        auth.signOut()
+        val intent= Intent(this, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
+    }
+
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (toggle.onOptionsItemSelected(item)){
